9b63e9e4095015864f97d7f908b2f96b2fdc78bf
qlwpc <245337588@qq.com>
2025-07-18 11:40:06 +0800

    add tree config“
    “

---
+++ run_scripts/tree.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 2
+#SBATCH -c 1
+#SBATCH --gres=gpu:2
+
+workspace=${HOME}/TG-Interpolation
+export PYTHONPATH=$PYTHONPATH:${workspace}/OLMo
+export HF_ENDPOINT=https://hf-mirror.com
+nvidia-smi
+wandb offline
+cd ${workspace}
+run_name=Tree_small
+torchrun --nproc-per-node=2 --master_port 15591 scripts/train.py \
+  ${workspace}/train_configs/tree.yaml \
+  --run_name=${run_name} \
+  --save_folder=${workspace}/saved_models/${run_name} \
+  --workspace=${workspace} 
