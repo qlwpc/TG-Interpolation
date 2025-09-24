241997c2cba2026287a04b4f8366637846f95bca
qlwpc <245337588@qq.com>
2025-09-24 11:10:31 +0800

    adding new eval and train scripts

--- evaluation/eval_scripts/xsum_term.sh
+++ evaluation/eval_scripts/xsum_term.sh
@@ -3,11 +3,11 @@
 #SBATCH -n 4
 #SBATCH -c 2
 #SBATCH --gres=gpu:4
-#SBATCH -t 120:00:00
 #SBATCH --mem-per-cpu=16384
+#SBATCH -t 80:00:00
 #SBATCH --partition=critical
 #SBATCH -A tukw-critical
-# --exclude=ai_gpu[26-35]
+#SBATCH --exclude=ai_gpu31
 
 workspace=${HOME}/TG-Interpolation
 export HF_ENDPOINT=https://hf-mirror.com
@@ -15,10 +15,10 @@ export PYTHONPATH=${PYTHONPATH}:${workspace}
 nvidia-smi
 wandb offline
 cd ${workspace}
-run_name=Terminal_test_xsum
+run_name=Terminal_finetune_xsum_lr5e-5_warmup50_3ep
 torchrun --nproc-per-node=4 --master_port 15590 scripts/train.py \
-  ${workspace}/evaluation/eval_configs/terminal.yaml \
+  ${workspace}/evaluation/xsum_configs/terminal.yaml \
   --run_name=${run_name} \
   --save_folder=${workspace}/saved_models/test_models/${run_name} \
   --workspace=${workspace} \
-  --load_path=/public/home/wangpch/TG-Interpolation/saved_models/test_models/Terminal_finetune_xsum_lr3e-4_warmup100_2ep/step896-unsharded
+  --load_path=/public/home/wangpch/TG-Interpolation/saved_models/Terminal-lr005-bs144/step34115-unsharded
