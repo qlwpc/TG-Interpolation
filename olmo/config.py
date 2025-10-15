Add autoconfig, Fixed beamsearch, fixed mask cpp, adding tgcausal bias, add xsum finetune, superglue finetune, left collate attention mask, tree shuffle mask, tg_aug mask, fixed datatools re expression

olmo/config.py
@@ -27,6 +27,7 @@ from torch.distributed.fsdp import MixedPrecision, ShardingStrategy
 from .aliases import PathOrStr
 from .exceptions import OLMoConfigurationError
 from .util import StrEnum
+from enum import Enum
 
 __all__ = [
     "ActivationType",
@@ -183,6 +184,10 @@ class LayerNormType(StrEnum):
     probably the fastest implementation.
     """
 
+class BeamSearchType(Enum):
+    default = 0
+    word_sync = 1
+    word_sync_dfs = 2
 
 class ActivationType(StrEnum):
     gelu = "gelu"
@@ -488,7 +493,7 @@ class ModelConfig(BaseConfig):
 
     transformer_grammar_type : str = "terminal"
     """
-    transformer grammar type in ["terminal", "tg", "tree", "tgproximal", "tgnomask", "tgheight"] 
+    transformer grammar type in ["terminal", "tg", "tree", "tgproximal", "tgnomask", "tgheight", "tree_shuffle"] 
     """
     mix_head_type: List[TGConfig] = field(default_factory=list)
 

olmo/data/__init__.py
@@ -11,30 +11,9 @@ from ..torch_util import barrier, get_global_rank, get_world_size
 from .collator import DataCollator
 from .iterable_dataset import IterableDataset
 from .memmap_dataset import MemMapDataset
-from .tg_mask import TG_attention_bias, KProximal_TG_attention_bias, Height_TG_attention_bias
+from .tg_mask import TG_attention_bias, KProximal_TG_attention_bias, Height_TG_attention_bias, SentencepieceVocab
 import torch 
 __all__ = ["MemMapDataset", "DataCollator", "IterableDataset", "build_eval_dataloader", "build_train_dataloader", "SentencepieceVocab", "get_TG_generate_bias_func"]
-
-class ChangeHead_bias:
-    def __init__(self, vocab_path:str = None, max_token_length:int = 2048, type:str = None) -> None:
-        if type=="mixA":
-            self.HeadA = TG_attention_bias(vocab_path, max_token_length)
-            self.HeadB = KProximal_TG_attention_bias(vocab_path, max_token_length, max_token_length)
-        elif type=="mixB":
-            self.HeadA = KProximal_TG_attention_bias(vocab_path, max_token_length, 200)
-            self.HeadB = Height_TG_attention_bias(vocab_path, max_token_length, 5)
-
-    def reset_state(self) -> None:
-        self.HeadA.reset_state()
-        self.HeadB.reset_state()
-    
-    def __call__(self, input_ids:torch.Tensor, update_state=False) -> Tuple[torch.Tensor, torch.Tensor]:
-        maskA, label_mask = self.HeadA(input_ids, update_state)
-        maskB, _  = self.HeadB(input_ids, update_state)
-        maskA = maskA.unsqueeze(0).expand(6, -1, -1)
-        maskB = maskB.unsqueeze(0).expand(6, -1, -1)
-        mask = torch.cat([maskA, maskB], dim=0)
-        return mask, label_mask
         
 class Soft_Alibilike_bias:
     def __init__(self, vocab_path:str = None, max_token_length:int = 2048, type:str = None) -> None:
@@ -63,10 +42,38 @@ class HeadMixingBias:
     def __call__(self, input_ids:torch.Tensor, update_state=False) -> Tuple[torch.Tensor, torch.Tensor]:
         masks, label_mask = [], None
         for gen_TG_bias, head_config in zip(self.TG_biases, self.config):
-            mask, label_mask = gen_TG_bias(input_ids, update_state)
+            mask, label_mask_head = gen_TG_bias(input_ids, update_state)
             masks.append(mask.unsqueeze(0).expand(head_config.n_heads, -1, -1))
+            if label_mask_head is not None:
+                label_mask = label_mask_head
         mask = torch.cat(masks, dim=0)
         return mask, label_mask
+
+class TGCausalBias:
+    def __init__(self, vocab_path:str, max_length:int) -> None:
+        self.vocab = SentencepieceVocab.from_vocab_file(vocab_path)
+        self.max_length = max_length
+        self.cur_length = 0
+        self.causal_cache = None
+    
+    def reset_state(self) -> None:
+        self.cur_length = 0
+
+    def __call__(self, input_ids:torch.Tensor, update_state=False) -> Tuple[torch.Tensor, torch.Tensor]:
+        pads_cnt = (input_ids == self.vocab.pad).sum().item()
+        mask = None
+        T = input_ids.shape[0]
+        remove_len = (self.cur_length + T - self.max_length) if self.cur_length + T > self.max_length else 0
+        pastT = (self.max_length - T)  if self.cur_length + T > self.max_length else self.cur_length
+        update_T = T - pads_cnt
+        if self.causal_cache is None:
+            self.causal_cache = torch.tril(
+                torch.ones(self.max_length, self.max_length, dtype=torch.bool),
+            )
+        mask = self.causal_cache[pastT:pastT+T, :pastT+T]
+        if update_state:
+            self.cur_length += - remove_len + update_T
+        return mask, None
 
 #TODO: promote parameter forwarding 
 def get_TG_generate_bias_func(train_config: TrainConfig, max_length:Optional[int] = None, TG_type:Optional[str]=None) -> TG_attention_bias:
@@ -80,20 +87,12 @@ def get_TG_generate_bias_func(train_config: TrainConfig, max_length:Optional[int
         generate_TG_attention_bias = HeadMixingBias(train_config.model.mix_head_type, train_config, max_length)
     elif TG_type=="tg":
         generate_TG_attention_bias = TG_attention_bias(vocab_path, max_length)
-    elif TG_type=="tgproximal":
-        generate_TG_attention_bias = KProximal_TG_attention_bias(vocab_path, max_length, train_config.model.tg_proximal_k)
-    elif TG_type=="tgheight":
-        generate_TG_attention_bias = Height_TG_attention_bias(vocab_path, max_length, train_config.model.tg_height_h)
-    elif TG_type=="tgnomask":
-        generate_TG_attention_bias = KProximal_TG_attention_bias(vocab_path, max_length, max_length)
-    elif TG_type=="mixA":
-        generate_TG_attention_bias = ChangeHead_bias(vocab_path, max_length, "mixA")
-    elif TG_type=="mixB":
-        generate_TG_attention_bias = ChangeHead_bias(vocab_path, max_length, "mixB")
-    elif TG_type=="soft_prox":
-        generate_TG_attention_bias = Soft_Alibilike_bias(vocab_path, max_length)
-    elif TG_type=="tgheightprox":
-        generate_TG_attention_bias = Height_TG_attention_bias(vocab_path, max_length, train_config.model.tg_height_h, train_config.model.tg_proximal_k)
+    elif TG_type[0:10]=="tgproximal":
+        generate_TG_attention_bias = KProximal_TG_attention_bias(vocab_path, max_length, train_config.model.tg_proximal_k, TG_type[-3:]=="aug")
+    elif TG_type[0:8]=="tgnomask":
+        generate_TG_attention_bias = KProximal_TG_attention_bias(vocab_path, max_length, max_length, TG_type[-3:]=="aug")
+    elif TG_type=="tgtree":
+        generate_TG_attention_bias = TGCausalBias(vocab_path, max_length)
     
     return generate_TG_attention_bias
 
@@ -187,7 +186,7 @@ def build_train_dataloader(
         )
     else:
         task_kwargs = {}
-        from ..eval.downstream import label_to_task_map
+        from ..eval.downstream import label_to_task_map, Super_GLUE
         task_class = label_to_task_map[train_config.finetune_task]
         if isinstance(task_class, tuple):
             task_class, task_kwargs = task_class
@@ -198,6 +197,8 @@ def build_train_dataloader(
                              vocab_path=train_config.tokenizer.vocabulary,
                              split="train",
                              **task_kwargs)  # type: ignore
+        if train_config.finetune_task in Super_GLUE:
+            collator = dataset.collate_fn
     
     work_dir = Path(train_config.save_folder) / "train_data"
     if get_global_rank() == 0:

olmo/data/collator.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn.functional as F
 
 from ..config import PaddingDirection, TrainConfig
-
+from .tg_mask import SentencepieceVocab
 __all__ = ["DataCollator"]
 
 
@@ -16,10 +16,15 @@ class DataCollator:
     pad_direction: PaddingDirection
     pad_token_id: int
     generate_attenion_mask: bool
+    shuffle_tree: bool = False
 
     @classmethod
     def from_train_config(cls, config: TrainConfig) -> DataCollator:
-        return cls(pad_direction=config.data.pad_direction, pad_token_id=config.model.pad_token_id, generate_attention_mask=False)
+        obj = cls(pad_direction=config.data.pad_direction, pad_token_id=config.model.pad_token_id, 
+                   generate_attention_mask=False, shuffle_tree=config.model.transformer_grammar_type=="tree_shuffle")
+        if obj.shuffle_tree:
+            obj.vocab = SentencepieceVocab.from_vocab_file(config.tokenizer.vocabulary)
+        return obj
 
     def __call__(self, items: Union[List[Dict[str, Any]], List[torch.Tensor]]) -> Dict[str, Any]:
         assert items
@@ -38,6 +43,8 @@ class DataCollator:
 
         for x in items:
             input_ids = x["input_ids"] if isinstance(x, dict) else x
+            if self.shuffle_tree:
+                input_ids = self.vocab.random_shuffle_tree(input_ids)
             if not isinstance(input_ids, torch.Tensor):
                 input_ids = torch.tensor(input_ids)
 

olmo/train.py
@@ -39,7 +39,8 @@ from .config import (
     ShardedCheckpointerType,
     SpeedMonitorConfig,
     TrainConfig,
-    EvaluatorType
+    EvaluatorType,
+    BeamSearchType
 )
 from .data import IterableDataset
 from .eval import Evaluator
@@ -776,8 +777,13 @@ class Trainer:
     def train_batch(self, batch: Dict[str, Any]) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
         # Split into micro-batches.
         micro_batches = self.split_batch(batch)
-        batch_size_in_loss_tokens = (self.get_labels(batch, self.cfg.model.pad_token_id) != self.cfg.model.pad_token_id).sum().item()
-        # batch_size_in_tokens = batch["input_ids"].numel()  # fixed: since TG or finetune task exist pad/non-loss tokens
+        if self.cfg.finetune_task is not None:
+            global_batch_size_in_loss_tokens = (self.get_labels(batch, self.cfg.model.pad_token_id) != self.cfg.model.pad_token_id).sum()
+            batch_size_in_loss_tokens = global_batch_size_in_loss_tokens.item()
+            dist.all_reduce(global_batch_size_in_loss_tokens)
+            device_loss_weight = get_world_size() * batch_size_in_loss_tokens / global_batch_size_in_loss_tokens.item()
+        else:
+            batch_size_in_loss_tokens = batch["input_ids"].numel()  # fixed: since TG or finetune task exist pad/non-loss tokens
 
         # In case this helps with memory utilization.
         del batch
@@ -815,6 +821,8 @@ class Trainer:
                         z_batch_loss += z_loss.detach()
 
                 # Run backward pass.
+                if self.cfg.finetune_task is not None:
+                    loss *= device_loss_weight
                 loss.backward()
 
             # Remove output hooks
@@ -1037,19 +1045,24 @@ class Trainer:
                     predictions = self.dist_model.module.generate(batch["input_ids"], 
                                                                    max_steps=evaluator.eval_loader.dataset.MAX_SUMMARY_LENGTH, 
                                                                    beam_size=6).token_ids
-                    predictions = predictions[:, 0, :]
+                    # predictions = predictions[0, 0, :].cpu().numpy()
+                    # predictions = evaluator.eval_loader.dataset.vocab.convert_treenpy_to_terminal(predictions)
+                    # predictions = torch.tensor(np.expand_dims(predictions, axis=0), device=self.device)
+                    predictions = predictions[:, 0, :].to(self.device)
                 else:
                     # currently only support eval_batch_size==1
                     predictions = self.dist_model.module.word_sync_beam_search(
                             vocab = evaluator.eval_loader.dataset.vocab,
                             past_input = batch["input_ids"][0],
-                            max_word_steps = evaluator.eval_loader.dataset.MAX_SUMMARY_LENGTH // 2,
+                            max_word_steps = evaluator.eval_loader.dataset.MAX_SUMMARY_LENGTH//2,
                             max_length = evaluator.eval_loader.dataset.MAX_SUMMARY_LENGTH, 
                             beam_size=6,
+                            generate_TG_bias=self.generate_TG_attention_bias, #get_TG_generate_bias_func(self.cfg),
+                            strategy=BeamSearchType.word_sync_dfs
                         )
                     predictions = predictions[0]["input_ids"].numpy()
                     predictions = evaluator.eval_loader.dataset.vocab.convert_treenpy_to_terminal(predictions)
-                    predictions = np.expand_dims(predictions, axis=0)
+                    predictions = torch.tensor(np.expand_dims(predictions, axis=0), device=self.device)
 
         
         evaluator.update_metrics(
@@ -1138,6 +1151,7 @@ class Trainer:
         self.optim.zero_grad(set_to_none=True)
         self.dist_model.eval()
         eval_metrics = {}
+        self.generate_TG_attention_bias = get_TG_generate_bias_func(self.cfg)
         for evaluator, eval_config in zip(self.evaluators, self.cfg.evaluators):
             log.info(f"Running evaluation for '{evaluator.label}'...")
             # Reset metrics.
@@ -1168,7 +1182,7 @@ class Trainer:
                     self.TG_doc_eval_step(eval_batch, evaluator)
                 elif evaluator.label == "syntactic_generalization":
                     self.SG_eval_step(eval_batch, evaluator)
-                elif evaluator.label == "xsum":
+                elif evaluator.type == EvaluatorType.rouge:
                     self.summarization_eval_step(eval_batch, evaluator)
                 else:
                     self.eval_step(eval_batch, evaluator)