5323dfea220b121f952d63771e4fb3fddab122bf
qlwpc <245337588@qq.com>
2025-10-19 23:07:44 +0800

    update test

--- scripts/init_cfg_and_sbatch.py
+++ scripts/init_cfg_and_sbatch.py
@@ -3,6 +3,7 @@ Run this to initialize a new training config to a file.
 """
 import logging
 import sys
+import os
 from pathlib import Path
 from typing import List, Optional
 from datetime import datetime
@@ -178,7 +179,7 @@ date
     print(f"已生成sbatch脚本: {script_filename}")
 
 def generate_config(save_path: Path, args_list: List[str], Device:str, modelname: str, task: str) -> None:
-    default_yaml_path = "./train_configs/terminal.yaml"
+    default_yaml_path = os.path.expanduser("~/TG-Interpolation/train_configs/terminal.yaml")
     override_args = {
         **Models[modelname],
         **train_params[task],
@@ -229,9 +230,10 @@ if __name__ == "__main__":
         save_path, args_list = sys.argv[1], sys.argv[2:]
     except IndexError:
         raise OLMoCliError(f"Usage: {sys.argv[0]} [SAVE_PATH] [OPTIONS]")
-    Device = "H800"
-    modelname = "nomask_mix_tree"
-    task = "pretrain_mix"
-    run_name = "TGnomask_mix_tree_pretrain"
+    Device = "SIST_TITAN"
+    modelname = "tree"
+    task = "SG"
+    run_name = "tree_test_SG"
+    load_path = "/home/wangpch/TG-Interpolation/saved_models/TG_test/step55457-unsharded"
     generate_config(Path(save_path), [clean_opt(s) for s in args_list], Device=Device, modelname=modelname, task=task)
-    generate_sbatch_content(config_path=Path(save_path), Device=Device, modelname=modelname, task=task, run_name=run_name)
+    generate_sbatch_content(config_path=Path(save_path), Device=Device, modelname=modelname, task=task, run_name=run_name, load_path=load_path)
