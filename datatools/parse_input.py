19d4f71ee8931d2e8254a43f9d01639b9b800232
qlwpc <245337588@qq.com>
2025-12-29 15:14:14 +0800

    optimize parse input

--- datatools/parse_input.py
+++ datatools/parse_input.py
@@ -13,10 +13,9 @@ import os
 import json
 import torch
 import numpy as np
+import gc
 
 from benepar import retokenization
-
-tokenizer = T5TokenizerFast.from_pretrained("t5-large")
 
 def my_retokenize(
     tokenizer,
@@ -128,6 +127,13 @@ def count_lines_linux_style(filename):
     reslist = result.stdout.split()
     return int(reslist[0]) if reslist!=[] else 0
 
+tokenizer = T5TokenizerFast.from_pretrained("t5-large")
+sentparser = spacy.load('en_core_web_md', disable=['tagger', 'attribute_ruler', 'lemmatizer', 'ner'])
+sentparser.add_pipe("set_custom_boundaries", before="parser")
+
+beneparser = benepar.Parser("benepar_en3_large")
+print(f"parser batch size is {beneparser.batch_size}")
+
 # benepar.download('benepar_en3_wsj')
 # nlp = spacy.load('en_core_web_md')
 # if spacy.__version__.startswith('2'):
@@ -141,30 +147,22 @@ def preprocess_text(text:str) -> str:
     return re.sub(r' $', '', re.sub(r'( )?\n +', '\n', re.sub(r'[^\S\n]+', ' ', text)))
 
 def split_long_sentence(tokens, max_len=256):
-    """
-    递归分割超过 max_len 的句子，优先按标点分割，否则硬切分
-    """
     if len(tokens) <= max_len:
         return [tokens]
-    
-    # 优先级从高到低的切分标点
+
     punctuations = ['.', '?', '!', ';', ':', ',']
     
-    # 在 max_len 位置向前查找最近的标点
     split_index = -1
     for i in range(max_len - 1, -1, -1):
         if tokens[i] in punctuations:
             split_index = i
             break
     
-    # 找到标点则按标点分割
     if split_index != -1:
         part1 = tokens[:split_index + 1]
         part2 = tokens[split_index + 1:]
         # print(f"lenpart1 = {len(part1)}, 2 = {len(part2)}")
         return [part1] + split_long_sentence(part2, max_len)
-    
-    # 找不到标点则硬切分
     else:
         part1 = tokens[:max_len]
         part2 = tokens[max_len:]
@@ -174,7 +172,9 @@ def split_long_sentence(tokens, max_len=256):
 def split_text_into_sents(text:str):
     text = preprocess_text(text)
     doc = sentparser(text)
-    return doc
+    sentences = [[str(token.text) for token in sent] for sent in doc.sents] # 提取字符串
+    del doc
+    return sentences
 
 def split_list_limit(sub_list, max_tokens=512):
     punctuations = {',', '.', '!', '?', ';', ':', '，', '。', '！', '？', '；', '：', '-'}
@@ -207,16 +207,10 @@ def split_list_limit(sub_list, max_tokens=512):
 
     return final_output
 
-def process_doc_into_maxlen(doc, max_len=512):
+def process_doc_into_maxlen(sents, max_len=512):
     final_sentences = []
-    for sent in doc.sents:
-        tokens = [token.text for token in sent]
+    for tokens in sents:
         final_sentences.extend(split_list_limit(tokens, max_tokens=max_len))
-        # if len(sent) <= max_len:
-        #     final_sentences.append(tokens)
-        # else:
-        #     segments = split_long_sentence(tokens, max_len)
-        #     final_sentences.extend(segments)
     return final_sentences
 
 # text = "Adam Afriyie (Windsor), Peter Aldous (Waveney), David Amess (Southend West), Stuart Andrew (Pudsey), Richard Bacon (Norfolk South), Steven Baker (Wycombe), Stephen Barclay (Cambridgeshire North East), John Baron (Basildon & Billericay), Gavin Barwell (Croydon Central), Guto Bebb (Aberconwy), Andrew Bingham (High Peak), Brian Binley (Northampton South), Crispin Blunt (Reigate), Graham Brady (Altrincham & Sale West), Andrew Bridgen (Leicestershire North West), Steve Brine (Winchester), Fiona Bruce (Congleton), Aidan Burley (Cannock Chase), Conor Burns (Bournemouth West), David Burrowes (Enfield Southgate), Dan Byles (Warwickshire North), Alun Cairns (Vale of Glamorgan), Bill Cash (Stone), Rehman Chishti (Gillingham & Rainham), Christopher Chope (Christchurch), James Clappison (Hertsmere), Geoffrey Cox (Devon West & Torridge), Tracey Crouch (Chatham & Aylesford), David Davies (Monmouth), Philip Davies (Shipley), David Davis (Haltemprice & Howden), Nick de Bois (Enfield North), Caroline Dinenage (Gosport), Nadine Dorries (Bedfordshire Mid),Richard Drax (Dorset South), James Duddridge (Rochford & Southend East), Graham Evans (Weaver Vale), Lorraine Fullbrook (South Ribble), Roger Gale (Thanet North), James Gray (Wiltshire North), Robert Halfon (Harlow), Simon Hart (Carmarthen West & Pembrokeshire South), Gordon Henderson (Sittingbourne & Sheppey), Sir Gerald Howarth (Aldershot), Stewart Jackson (Peterborough), Bernard Jenkin (Harwich & Essex North), Gareth Johnson (Dartford), Marcus Jones (Nuneaton), Daniel Kawczynski (Shrewsbury & Atcham), Chris Kelly (Dudley South), Simon Kirby (Brighton Kemptown), Andrea Leadsom (Northamptonshire South), Jessica Lee (Erewash), Phillip Lee (Bracknell), Edward Leigh (Gainsborough), Charlotte Leslie (Bristol North West), Julian Lewis (New Forest East), Ian Liddell-Grainger (Bridgwater & Somerset West), Jonathan Lord (Woking), Tim Loughton (Worthing East & Shoreham), Karen Lumley (Redditch), Jason McCartney (Colne Valley), Karl McCartney (Lincoln), Stephen McPartland (Stevenage), Anne Main (St Albans), Paul Maynard (Blackpool North & Cleveleys), Mark Menzies (Fylde), Patrick Mercer (Newark), Stephen Metcalfe (Basildon South & Thurrock East), Nigel Mills (Amber Valley), David Morris (Morecambe & Lunesdale), James Morris (Halesowen & Rowley Regis), Caroline Nokes (Romsey & Southampton North), David Nuttall (Bury North), Matthew Offord (Hendon), Eric Ollerenshaw (Lancaster & Fleetwood), Priti Patel (Witham), John Penrose (Weston-Super-Mare), Andrew Percy (Brigg & Goole), Stephen Phillips (Sleaford & North Hykeham), Chris Pincher (Tamworth), Dominic Raab (Esher & Walton), Mark Reckless (Rochester & Strood), John Redwood (Wokingham), Jacob Rees-Mogg (Somerset North East), Laurence Robertson (Tewkesbury), Andrew Rosindell (Romford), David Ruffley (Bury St Edmunds), Andrew Selous (Bedfordshire South West), Alec Shelbrooke (Elmet & Rothwell), Sir Richard Shepherd (Aldridge-Brownhills), Henry Smith (Crawley), Mark Spencer (Sherwood), Andrew Stephenson (Pendle), John Stevenson (Carlisle), Iain Stewart (Milton Keynes South), Gary Streeter (Devon South West), Mel Stride (Devon Central), Julian Sturdy (York Outer), Sir Peter Tapsell (Louth & Horncastle), Justin Tomlinson (Swindon North), David Tredinnick (Bosworth), Andrew Turner (Isle of Wight), Martin Vickers (Cleethorpes), Charles Walker (Broxbourne), Robin Walker (Worcester), James Wharton (Stockton South), Heather Wheeler (Derbyshire South), Chris White (Warwick & Leamington), Craig Whittaker (Calder Valley), John Whittingdale (Maldon), Bill Wiggin (Herefordshire North), Dr Sarah Wollaston (Totnes), Nadhim Zahawi (Stratford-on-Avon). The two Tory tellers were Peter Bone (Wellingborough) and Philip Hollobone (Kettering)."
@@ -251,7 +245,7 @@ class batch_buffer:
         if is_end:
             self.file.write(self.doc_to_write)
             self.doc_to_write = ""
-            pbar.update(1)
+            self.pbar.update(1)
 
     def parse_batch(self):
         if len(self.batches)==0:
@@ -286,7 +280,7 @@ def load_shrunk_dataset(directory_path, file_pattern:str = None):
     data_files = []
     regex = re.compile(file_pattern)
     for filename in os.listdir(directory_path):
-        if regex.match(filename):  # 或使用 match() 从开头匹配
+        if regex.match(filename):
             data_files.append(os.path.join(directory_path,filename))
     print(data_files)
     if not data_files:
@@ -398,19 +392,12 @@ def prepare_dataset(config:str):
 
     return filename, prepared_ds
 
-#[, 'CC-MAIN-2013-48', 'CC-MAIN-2014-10', 'CC-MAIN-2014-15', 'CC-MAIN-2014-23', 'CC-MAIN-2014-35', 'CC-MAIN-2014-41', 'CC-MAIN-2014-42', 'CC-MAIN-2014-49', 'CC-MAIN-2014-52', 'CC-MAIN-2015-06', 'CC-MAIN-2015-11', 'CC-MAIN-2015-14', 'CC-MAIN-2015-18', 'CC-MAIN-2015-22', 'CC-MAIN-2015-27', 'CC-MAIN-2015-32', 'CC-MAIN-2015-35', 'CC-MAIN-2015-40', 'CC-MAIN-2015-48', 'CC-MAIN-2016-07', 'CC-MAIN-2016-18', 'CC-MAIN-2016-22', 'CC-MAIN-2016-26', 'CC-MAIN-2016-30', 'CC-MAIN-2016-36', 'CC-MAIN-2016-40', 'CC-MAIN-2016-44', 'CC-MAIN-2016-50', 'CC-MAIN-2017-04', 'CC-MAIN-2017-09', 'CC-MAIN-2017-13', 'CC-MAIN-2017-17', 'CC-MAIN-2017-22', 'CC-MAIN-2017-26', 'CC-MAIN-2017-30', 'CC-MAIN-2017-34', 'CC-MAIN-2017-39', 'CC-MAIN-2017-43', 'CC-MAIN-2017-47', 'CC-MAIN-2017-51', 'CC-MAIN-2018-05', 'CC-MAIN-2018-09', 'CC-MAIN-2018-13', 'CC-MAIN-2018-17', 'CC-MAIN-2018-22', 'CC-MAIN-2018-26', 'CC-MAIN-2018-30', 'CC-MAIN-2018-34', 'CC-MAIN-2018-39', 'CC-MAIN-2018-43', 'CC-MAIN-2018-47', 'CC-MAIN-2018-51', 'CC-MAIN-2019-04', 'CC-MAIN-2019-09', 'CC-MAIN-2019-13', 'CC-MAIN-2019-18', 'CC-MAIN-2019-22', 'CC-MAIN-2019-26', 'CC-MAIN-2019-30', 'CC-MAIN-2019-35', 'CC-MAIN-2019-39', 'CC-MAIN-2019-43', 'CC-MAIN-2019-47', 'CC-MAIN-2019-51', 'CC-MAIN-2020-05', 'CC-MAIN-2020-10', 'CC-MAIN-2020-16', 'CC-MAIN-2020-24', 'CC-MAIN-2020-29', 'CC-MAIN-2020-34', 'CC-MAIN-2020-40', 'CC-MAIN-2020-45', 'CC-MAIN-2020-50', 'CC-MAIN-2021-04', 'CC-MAIN-2021-10', 'CC-MAIN-2021-17', 'CC-MAIN-2021-21', 'CC-MAIN-2021-25', 'CC-MAIN-2021-31', 'CC-MAIN-2021-39', 'CC-MAIN-2021-43', 'CC-MAIN-2021-49', 'CC-MAIN-2022-05', 'CC-MAIN-2022-21', 'CC-MAIN-2022-27', 'CC-MAIN-2022-33', 'CC-MAIN-2022-40', 'CC-MAIN-2022-49', 'CC-MAIN-2023-06', 'CC-MAIN-2023-14', 'CC-MAIN-2023-23', 'CC-MAIN-2023-40', 'CC-MAIN-2023-50']
-if __name__=="__main__":
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_list', type=str)  # 接收单个字符串
+    parser.add_argument('--input_list', type=str)
     parser.add_argument('--start_index', type=int, default=0)
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-    sentparser = spacy.load('en_core_web_md')
-    sentparser.add_pipe("set_custom_boundaries", before="parser")
-
-    beneparser = benepar.Parser("benepar_en3_large")
-    print(f"parser batch size is {beneparser.batch_size}")
 
     print(args.input_list)
     result_list = args.input_list.split(',') if args.input_list else []
@@ -419,9 +406,6 @@ if __name__=="__main__":
     for split in config:
         filename, ds = prepare_dataset(split)
         print(f"len dataset is {len(ds)}")
-        # exit(0)
-        # ds = load_dataset("permutans/fineweb-bbc-news", split)
-        # ds = ds["train"]
         totallen = len(ds)
         print(totallen)
         logging.info(f"start parsing {split}")
@@ -444,8 +428,13 @@ if __name__=="__main__":
                 Buffer.append_batch(split_sents)
                 # print(f"input is {split_sents}")
                 index += 1
+                if index % 200 == 0:
+                    gc.collect()
             Buffer.parse_batch()
             print("end parse")
         index = 0
                 
     logging.info("finished")
+
+if __name__=="__main__":
+    main()
